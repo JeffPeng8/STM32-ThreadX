ec794daed68c85e768b4a2ba1b7dcad24c9ddecb
Jeffrey Peng <jeffreyp@a-concepts.com>
2025-08-15 15:15:13 -0500

    Tested & Verified Code

--- Core/Src/app_threadx.c
+++ Core/Src/app_threadx.c
@@ -77,6 +77,7 @@ void StartLowTask(ULONG thread_input);
 UINT App_ThreadX_Init(VOID *memory_ptr)
 {
   UINT ret = TX_SUCCESS;
+
   /* USER CODE BEGIN App_ThreadX_MEM_POOL */
 
   /* USER CODE END App_ThreadX_MEM_POOL */
